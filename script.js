49b1606dd7283fea2ee345a8f3802871cea74eed
oseias447 <daniluizez@gmail.com>
2023-07-10 16:49:41 -0300

    alterado arquivo script.js

--- script.js
+++ script.js
@@ -9,16 +9,8 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a image light
     img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute(
-      "alt",
-      "Foto de Danile Zortea sorrindo, com cabelo solto e regata preta"
-    )
   } else {
     // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute(
-      "alt",
-      "Foto de Daniele Zortea sorrindo, de cabelo solto e camisa azul marinho com estampa."
-    )
   }
 }
