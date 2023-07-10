5dafc31d93385bc4df83b650e37fb3dad524634c
oseias447 <daniluizez@gmail.com>
2023-07-10 14:21:25 -0300

    :tada: initial commit

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light");
+  html.classList.toggle("light")
 
   //pegar a tag img
-  const img = document.querySelector("#profile img");
+  const img = document.querySelector("#profile img")
 
   //substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a image light
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Danile Zortea sorrindo, com cabelo solto e regata preta"
+    )
   } else {
     // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Daniele Zortea sorrindo, de cabelo solto e camisa azul marinho com estampa."
+    )
   }
 }
